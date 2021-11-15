6126bba3f2c01e57e71f866af72156b85936b9ea
romeo <ii.kim@fitpet.co.kr>
2021-11-16 01:28:13 +0900

    Repository Custom 추가(기능구현 아님, 구조 수정)

---
+++ src/main/java/farmskin/library/repository/BookRepositoryCustom.java
@@ -0,0 +1,12 @@
+package farmskin.library.repository;
+
+import farmskin.library.domain.Book;
+import farmskin.library.domain.type.Category;
+import java.util.List;
+
+public interface BookRepositoryCustom {
+
+  List<Book> findByTitleContainingIgnoreCaseOrAuthorContainingIgnoreCase(String title, String author);
+
+  List<Book> findByCategory(Category category);
+}
